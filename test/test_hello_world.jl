8b301945bcdd9d9d1513dc35277859aff926ee09
kourosh <krshz247@outlook.com>
2021-12-12 10:15:23 +0100

    Remove Hello World files.

--- test/test_hello_world.jl
+++
@@ -1,8 +0,0 @@
-# This file is a part of RunStatistics.jl, licensed under the MIT License (MIT).
-
-using RunStatistics
-using Test
-
-@testset "hello_world" begin
-    @test RunStatistics.hello_world() == 42
-end
