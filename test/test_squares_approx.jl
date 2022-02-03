ed99f07f81c43ca0dbc080ebba94fd3c770be932
Micki-D <michael.dudkowiak@tum.de>
2022-02-03 12:15:52 +0100

    fix bugs in t_obs, squares_approx, adjust guide section on accuracy

--- test/test_squares_approx.jl
+++ test/test_squares_approx.jl
@@ -6,12 +6,26 @@ using Test
 @testset "squares_approx" begin
 
     T_obs = 20
-    N = 30
-    n = 100
-    epsp = 10^(-2)
+    L = 800
+    N = 80
+    n = 10
+    Ns = [N, n]
+    epsp_1 = 10^(-2)
+    epsp_2 = 0
+        
 
-    @test squares_pvalue_approx(T_obs, N, n, epsp) ≈ 0.24164150728705625
-    @test squares_cdf_approx(T_obs, N, n, epsp) ≈ 0.7583584927129438
+    @test RunStatistics.squares_pvalue_approx(T_obs, Ns, epsp_1) ≈ 0.07082230261169509
+    @test RunStatistics.squares_cdf_approx(T_obs, Ns, epsp_1) ≈ 0.9291776973883049
+
+    @test RunStatistics.squares_cdf_approx(T_obs, L, epsp_1) ≈ 0.9291776973883049
+    @test RunStatistics.squares_pvalue_approx(T_obs, L, epsp_1) ≈ 0.07082230261169509
+
+
+    @test RunStatistics.squares_pvalue_approx(T_obs, Ns, epsp_2) == 0.0708223271587789
+    @test RunStatistics.squares_cdf_approx(T_obs, Ns, epsp_2) == 0.9291776728412211
+
+    @test RunStatistics.squares_cdf_approx(T_obs, L, epsp_2) == 0.9291776728412211
+    @test RunStatistics.squares_pvalue_approx(T_obs, L, epsp_2) == 0.0708223271587789
 end
 
 # I measured the timing for N=96 with the exact and approximate solution. See if julia is really faster. It seems some experiment with threading was done but without threads, I'd be surprised if it's faster than the C++ code built in release mode
